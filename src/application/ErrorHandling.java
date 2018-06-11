eae7af6554e6363b9b1755896a095a0fdef61ca9
MeugenoM <meugenom@gmail.com>
2018-06-11 14:53:19 +0200

    Create ErrorHandling.java

---
+++ src/application/ErrorHandling.java
@@ -0,0 +1,18 @@
+package application;
+
+public class ErrorHandling {
+
+	public static void main(String[] args) throws Exception {
+
+		try {
+
+		}
+
+		catch (Exception exception) {
+
+			System.err.println(exception);
+
+		}
+	}
+
+}
