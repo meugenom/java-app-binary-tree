c003a7dd6f87f38e0f04925307e2d6ca165a3b9d
MeugenoM <meugenom@gmail.com>
2018-06-11 14:51:57 +0200

    Update Constants.java

--- src/application/Constants.java
+++ src/application/Constants.java
@@ -5,26 +5,27 @@ public final class Constants {
     public  static String ApplicationName = "Pythagoras Tree ";
     public  static String Version = "v. 0.1";
     
+    //dimension of scene
     public static int SceneWidth = 800;
     public static int SceneHeight = 600;
     
+    //dimension of canvas
     public static int CanvasWidth = 800;
     public static int CanvasHeight = 600;
     
     
-    
+    //start point from where gets tree
     public  static double  StartPointX = 400;
     public  static double  StartPointY = 520;
-    public static double StartLength = 70;
     
-    //public  static double  WidthShape = 70.0;
-    //public  static double  HeightShape = 70.0;
-        
-    public  static int  Angle = 45;
-    public  static int  Iteration = 10;
+    public static double StartLength = 70;    
+           
+    //for future realization 
+    public  static int  Iteration = 3;
     public  static boolean  Random = false;
     public  static int  Delay = 1;
     
+    //orientation of vector
     public static double ConvertAngle = Math.PI/180;    
-    
+    public  static double  Angle = 45.0;
 }
