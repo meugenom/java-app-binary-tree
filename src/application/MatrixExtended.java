7cc930d009a1b06776fedd95d58e01c03ab2250b
MeugenoM <meugenom@gmail.com>
2018-06-11 14:50:38 +0200

    Create MatrixExtended.java

---
+++ src/application/MatrixExtended.java
@@ -0,0 +1,38 @@
+package application;
+
+import libraries.Matrix;
+
+public class MatrixExtended extends Matrix{
+
+	Matrix rotateMatrix = new Matrix(2,2);
+	
+	public MatrixExtended(int m, int n) throws Exception {
+		super(m, n);
+	}
+		 
+	public void rotateLeft(double angle) {
+		rotateMatrix.set(0, 0,   Math.cos(Constants.ConvertAngle*angle));		
+		rotateMatrix.set(1, 0, - Math.sin(Constants.ConvertAngle*angle));
+		rotateMatrix.set(0, 1,   Math.sin(Constants.ConvertAngle*angle));
+		rotateMatrix.set(1, 1,   Math.cos(Constants.ConvertAngle*angle));
+		
+		double aX = this.get(0, 0)*rotateMatrix.get(0, 0) + this.get(0, 1)*rotateMatrix.get(0, 1); 
+		double aY = this.get(0, 0)*rotateMatrix.get(1, 0) + this.get(0, 1)*rotateMatrix.get(1, 1);
+				
+		this.set(0, 0, aX);
+		this.set(0, 1, aY);		
+	}
+	
+	public void rotateRight(double angle) {
+		rotateMatrix.set(0, 0,   Math.cos(Constants.ConvertAngle*angle));		
+		rotateMatrix.set(1, 0, - Math.sin(Constants.ConvertAngle*angle));
+		rotateMatrix.set(0, 1,   Math.sin(Constants.ConvertAngle*angle));
+		rotateMatrix.set(1, 1,   Math.cos(Constants.ConvertAngle*angle));
+		
+		double aX = this.get(0, 0)*rotateMatrix.get(0, 0) + this.get(0, 1)*rotateMatrix.get(0, 1); 
+		double aY = this.get(0, 0)*rotateMatrix.get(1, 0) + this.get(0, 1)*rotateMatrix.get(1, 1);
+				
+		this.set(0, 0, aY);
+		this.set(0, 1, aX);		
+	}
+}
